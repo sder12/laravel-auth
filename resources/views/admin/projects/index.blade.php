ec4a36d0c3511b1c0fd069e8734b2247bf98be2e
sder12 <sara.cetto@gmail.com>
2023-01-11 15:46:06 +0100

    Delete with modal boostrap

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -45,15 +45,53 @@
                                     <a class="btn btn-warning" href="{{ route('admin.projects.edit', $project->slug) }}">
                                         <i class="fa-solid fa-pencil"></i>
                                     </a>
-                                    <form class="d-inline-block"
-                                        action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST">
-                                        @method('DELETE')
-                                        @csrf
+                                    {{-- 1. CANCELLAZIONE IMMEDIATA --}}
+                                    {{-- <form class="d-inline-block"
+                                            action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST">
+                                            @method('DELETE')
+                                            @csrf
 
-                                        <button class="btn btn-danger">
-                                            <i class="fa-solid fa-trash"></i>
-                                        </button>
-                                    </form>
+                                            <button class="btn btn-danger">
+                                                <i class="fa-solid fa-trash"></i>
+                                            </button>
+                                        </form> --}}
+
+                                    {{-- 2. CANCELLAZIONE CON BOOSTRAP MODAL IN OGNI ELEMENTO, SEMPLICE MA CODICE NON OTTIMALE --}}
+                                    <!-- Button trigger modal -->
+                                    <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                        data-bs-target="#delete-project-{{ $project->id }}">
+                                        <i class="fa-solid fa-trash"></i>
+                                    </button>
+
+                                    <!-- Modal della conferma prima della cancellazione -->
+                                    <div class="modal fade" id="delete-project-{{ $project->id }}" tabindex="-1"
+                                        aria-labelledby="delete-label-{{ $project->id }}" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h3 class="modal-title fs-5" id="delete-label-{{ $project->id }}">
+                                                        Are you sure you want to cancel {{ $project->title }}?</h3>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-bs-dismiss="modal">No</button>
+                                                    <form class="d-inline-block"
+                                                        action="{{ route('admin.projects.destroy', $project->slug) }}"
+                                                        method="POST">
+                                                        @method('DELETE')
+                                                        @csrf
+
+                                                        <button class="btn btn-danger">
+                                                            Yes, cancel
+                                                        </button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
                                 </td>
                             </tr>
                         @endforeach
