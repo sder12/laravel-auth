1023ab5cff7a16171a2234563d3240821182b4b1
sder12 <sara.cetto@gmail.com>
2023-01-13 18:11:21 +0100

    Creation new col migration labeled cover_img + new item in fillable

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,7 +10,7 @@ class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'description', 'utilized_programs', 'creation_year', 'slug'];
+    protected $fillable = ['title', 'description', 'utilized_programs', 'creation_year', 'slug', 'cover_img',];
 
     public static function generateSlug($title)
     {
